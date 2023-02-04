d333f92285f1943ab0856ae84baeba53dcc1de21
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-02-04 23:15:02 +0530

    feat: initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,22 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Prashanna Rajbhandari on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            NavigationView{
+                ListView()
+            }
+            
+            
+        }
+    }
+}
